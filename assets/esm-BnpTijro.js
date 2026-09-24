7aef9412aa6be3da78fe572d06de5f5ff0e442c9
alexrey11 <alejandrocuestarey@gmail.com>
2026-09-24 16:31:32 -0400

    Update: exports Android 13

--- assets/esm-BnpTijro.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D7CZO6n4.js","./index-DUp9phYB.js","./index-DgkchJYR.css"])))=>i.map(i=>d[i]);
-import{l as e,n as t}from"./index-DUp9phYB.js";var n=e(`Share`,{web:()=>t(()=>import(`./web-D7CZO6n4.js`).then(e=>new e.ShareWeb),__vite__mapDeps([0,1,2]),import.meta.url)});export{n as Share};
